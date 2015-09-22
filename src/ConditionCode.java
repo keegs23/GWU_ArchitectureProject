2a13275fa01cfdedb0444b286e6a0bb7a2f07e93
568666 <568666@BAHCND5152L13.resource.ds.bah.com>
2015-09-22 19:19:56 -0400

    ConditionCode and Register Constructors

--- src/ConditionCode.java
+++ src/ConditionCode.java
@@ -1,4 +1,7 @@
 public class ConditionCode
 {
-	
+    private static final String OVERFLOW = "0";
+    private static final String UNDERFLOW = "1";
+    private static final String DIVZERO = "2";
+    private static final String EQUALORNOT = "3"; 
 }
