685fc669901b117897e0f922695e9c720d47e309
kruphix <chihoon22@gmail.com>
2015-09-27 23:45:08 -0400

    minicomputer remove unused imports

--- src/MiniComputer.java
+++ src/MiniComputer.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
-import java.util.TreeMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -174,10 +172,6 @@ public class MiniComputer
 		return memory;
 	}
 	
-	public static void main(String[] args)
-	{
-		// TODO: mimic flowchart from Lecture 1
-	}
 	
 	/**
 	 * Loads the ROM contents.
