09aba968aee627e34f8409e0e2c9de9b2f93ae87
kwana37 <kwan_anne@gwu.edu>
2015-09-28 23:15:10 -0400

    Design notes corrections, small change to Simple Documentation, quickly change ConditionCode to match design notes

--- src/ConditionCode.java
+++ src/ConditionCode.java
@@ -1,7 +1,7 @@
-public class ConditionCode
+public enum ConditionCode
 {
-    private static final String OVERFLOW = "0";
-    private static final String UNDERFLOW = "1";
-    private static final String DIVZERO = "2";
-    private static final String EQUALORNOT = "3"; 
+    OVERFLOW,
+    UNDERFLOW,
+    DIVZERO,
+    EQUALORNOT
 }
