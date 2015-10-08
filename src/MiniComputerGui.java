951e407c85186da892bd18da72ed2d00c39d9975
kruphix <chihoon22@gmail.com>
2015-10-08 00:32:01 -0400

    Added Halt button, added other registers to table, added console printer and keyboard

--- src/MiniComputerGui.java
+++ src/MiniComputerGui.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -15,6 +16,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
@@ -29,52 +31,67 @@ public class MiniComputerGui extends JFrame implements ActionListener {
 	private MiniComputer cpu;
 	private Container mainPanel;
 	private JPanel leftPanel;
+	private JPanel centerPanel;
 	private JPanel rightPanel;
 	private JPanel iplPanel;
 	private JPanel instructionPanel;
 	private JPanel pcPanel;
 	private JPanel registerPanel;
 	private JPanel memoryPanel;
+	private JPanel consolePrinterPanel;
+	private JPanel consoleKeyboardPanel;
 	private JLabel[] indicators;
 	private JToggleButton[] toggles;
 	private JButton instructionLoadButton;
 	private JTextField pcInput;
 	private JButton iplButton;
+	private JButton haltButton;
 	private JButton singleStepButton;
 	private DefaultTableModel registerModel;
 	private DefaultTableModel memoryModel;
 	private JTable registerTable;
 	private JTable memoryTable;
+	private JTextArea consolePrinterOutput;
+	private JTextArea consoleKeyboardInput;
 
 	public MiniComputerGui() {
 
 		cpu = new MiniComputer();
 		mainPanel = getContentPane();
 		leftPanel = new JPanel();
+		centerPanel = new JPanel();
 		rightPanel = new JPanel();
 		iplPanel = new JPanel();
 		instructionPanel = new JPanel();
 		pcPanel = new JPanel();
 		registerPanel = new JPanel();
 		memoryPanel = new JPanel();
+		consolePrinterPanel = new JPanel();
+		consoleKeyboardPanel = new JPanel();
 		indicators = new JLabel[INSTRUCTION_SIZE];
 		toggles = new JToggleButton[INSTRUCTION_SIZE];
 		instructionLoadButton = new JButton("Load Instruction");
 		pcInput = new JTextField(10);
 		iplButton = new JButton("IPL");
+		haltButton = new JButton("Halt");
 		singleStepButton = new JButton("Single Step");
 		registerModel = new DefaultTableModel();
 		memoryModel = new DefaultTableModel();
 		registerTable = new JTable(registerModel);
 		memoryTable = new JTable(memoryModel);
+		consolePrinterOutput = new JTextArea();
+		consoleKeyboardInput = new JTextArea();
 		
 		initUI();
     }
 
     private void initUI() {
 
+    	final int WINDOW_WIDTH = 1000;
+    	final int WINDOW_HEIGHT = 800;
+    	
     	setTitle("The Mini Computer");
-        setSize(1100, 1000);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
@@ -86,11 +103,14 @@ public class MiniComputerGui extends JFrame implements ActionListener {
         addToPcPanel();
         addToRegisterPanel();
         addToMemoryPanel();
+        addToConsolePrinterPanel();
+        addToConsoleKeyboardPanel();
         
         initRegisterTable();
         initMemoryTable();
         
         addToLeftPanel();
+        addToCenterPanel();
         addToRightPanel();
         addToMainPanel();
         
@@ -98,14 +118,17 @@ public class MiniComputerGui extends JFrame implements ActionListener {
     
     private void setPanelLayouts() {
     	
-    	mainPanel.setLayout(new GridLayout(1, 2));
+    	mainPanel.setLayout(new GridLayout(1, 3));
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
         rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
         iplPanel.setLayout(new FlowLayout());
         instructionPanel.setLayout(new GridLayout(0, 2));
         pcPanel.setLayout(new FlowLayout());
-        registerPanel.setLayout(new FlowLayout());
-        memoryPanel.setLayout(new FlowLayout());
+        registerPanel.setLayout(new BorderLayout());
+        memoryPanel.setLayout(new BorderLayout());
+        consolePrinterPanel.setLayout(new BorderLayout());
+        consoleKeyboardPanel.setLayout(new BorderLayout());
     }
     
     private void setPanelBorders() {
@@ -115,14 +138,25 @@ public class MiniComputerGui extends JFrame implements ActionListener {
         pcPanel.setBorder(new TitledBorder("Program Counter"));
         registerPanel.setBorder(new TitledBorder("Registers"));
         memoryPanel.setBorder(new TitledBorder("Memory in Use"));
+        consolePrinterPanel.setBorder(new TitledBorder("Console Printer"));
+        consoleKeyboardPanel.setBorder(new TitledBorder("Console Keyboard"));
     }
     
     private void addToIplPanel() {
     	
-    	iplButton.setPreferredSize(new Dimension(200,100));
-    	iplButton.setFont(new Font("Arial", Font.PLAIN, 40));
+    	final int BUTTON_WIDTH = 150;
+    	final int BUTTON_HEIGHT = 75;
+    	final int FONT_SIZE = 30;
+    	
+    	iplButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
+    	iplButton.setFont(new Font("Arial", Font.PLAIN, FONT_SIZE));
     	iplButton.addActionListener(this);
         iplPanel.add(iplButton);
+        
+        haltButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
+    	haltButton.setFont(new Font("Arial", Font.PLAIN, FONT_SIZE));
+    	haltButton.addActionListener(this);
+        iplPanel.add(haltButton);
     }
     
     private void addToInstructionPanel() {
@@ -166,18 +200,46 @@ public class MiniComputerGui extends JFrame implements ActionListener {
         memoryPanel.add(new JScrollPane(memoryTable));
     }
     
+    private void addToConsolePrinterPanel() {
+    	
+    	consolePrinterOutput.setEditable(false);
+    	consolePrinterOutput.setLineWrap(true);
+    	consolePrinterOutput.setWrapStyleWord(true);
+    	consolePrinterPanel.add(new JScrollPane(consolePrinterOutput));
+    }
+    
+    private void addToConsoleKeyboardPanel() {
+    	
+    	consoleKeyboardInput.setLineWrap(true);
+    	consoleKeyboardInput.setWrapStyleWord(true);
+    	consoleKeyboardPanel.add(new JScrollPane(consoleKeyboardInput));
+    }
+    
     private void initRegisterTable() {
+    	
+    	final String SIXTEEN_ZEROS = "0000000000000000";
+    	final String TWELVE_ZEROS = "000000000000";
+    	final String FOUR_ZEROS = "0000";
     	
     	registerModel.setRowCount(0);
     	registerModel.addColumn("Register");
         registerModel.addColumn("Value (base-2)");
-        registerModel.addRow(new Object[]{"R0", "0000 0000 0000 0000"});
-        registerModel.addRow(new Object[]{"R1", "0000 0000 0000 0000"});
-        registerModel.addRow(new Object[]{"R2", "0000 0000 0000 0000"});
-        registerModel.addRow(new Object[]{"R3", "0000 0000 0000 0000"});
-        registerModel.addRow(new Object[]{"X1", "0000 0000 0000 0000"});
-        registerModel.addRow(new Object[]{"X2", "0000 0000 0000 0000"});
-        registerModel.addRow(new Object[]{"X3", "0000 0000 0000 0000"});
+        registerModel.addRow(new Object[]{"PC", TWELVE_ZEROS});
+        registerModel.addRow(new Object[]{"CC", FOUR_ZEROS});
+        registerModel.addRow(new Object[]{"IR", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"MAR", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"MBR", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"MSR", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"MFR", FOUR_ZEROS});
+        registerModel.addRow(new Object[]{"IAR", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"IRR", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"R0", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"R1", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"R2", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"R3", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"X1", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"X2", SIXTEEN_ZEROS});
+        registerModel.addRow(new Object[]{"X3", SIXTEEN_ZEROS});
     }
     
     private void initMemoryTable() {
@@ -195,15 +257,22 @@ public class MiniComputerGui extends JFrame implements ActionListener {
         leftPanel.add(pcPanel);
     }
     
+    private void addToCenterPanel() {
+    	
+    	centerPanel.add(registerPanel);
+        centerPanel.add(memoryPanel);
+    }
+    
     private void addToRightPanel() {
     	
-    	rightPanel.add(registerPanel);
-        rightPanel.add(memoryPanel);
+    	rightPanel.add(consolePrinterPanel);
+    	rightPanel.add(consoleKeyboardPanel);
     }
     
     private void addToMainPanel() {
     	
     	mainPanel.add(leftPanel);
+        mainPanel.add(centerPanel);
         mainPanel.add(rightPanel);
     }
     
@@ -245,6 +314,15 @@ public class MiniComputerGui extends JFrame implements ActionListener {
     public void populateRegisterTable() {
     	
     	registerModel.setRowCount(0);
+    	registerModel.addRow(new Object[]{"PC", cpu.getPC().getBitValue().getValue()});
+        registerModel.addRow(new Object[]{"CC", cpu.getCC().getBitValue().getValue()});
+        registerModel.addRow(new Object[]{"IR", cpu.getIR().getBitValue().getValue()});
+        registerModel.addRow(new Object[]{"MAR", cpu.getMAR().getBitValue().getValue()});
+        registerModel.addRow(new Object[]{"MBR", cpu.getMBR().getBitValue().getValue()});
+        registerModel.addRow(new Object[]{"MSR", cpu.getMSR().getBitValue().getValue()});
+        registerModel.addRow(new Object[]{"MFR", cpu.getMFR().getBitValue().getValue()});
+        registerModel.addRow(new Object[]{"IAR", cpu.getIAR().getBitValue().getValue()});
+        registerModel.addRow(new Object[]{"IRR", cpu.getIRR().getBitValue().getValue()});
         registerModel.addRow(new Object[]{"R0", cpu.getR(0).getBitValue().getValue()});
         registerModel.addRow(new Object[]{"R1", cpu.getR(1).getBitValue().getValue()});
         registerModel.addRow(new Object[]{"R2", cpu.getR(2).getBitValue().getValue()});
@@ -292,17 +370,19 @@ public class MiniComputerGui extends JFrame implements ActionListener {
     	else { // else check the other buttons
     		if (src == iplButton) {
         		runIpl();
+    		} else if (src == haltButton) {
+    			runHalt();
         	} else if (src == instructionLoadButton) {
         		runInstructionLoad();
         	} else if (src == singleStepButton) {
         		runSingleStep();
         	} else {
-        		System.out.println("SOMETHING WENT WRONG!");
+        		System.out.println("ERROR: UNKNOWN EVENT SOURCE!");
         	}
     	}	
     }
     
-    public void runIpl() {
+    private void runIpl() {
     	
     	System.out.println("IPL BUTTON CLICKED!");
 		
@@ -311,17 +391,24 @@ public class MiniComputerGui extends JFrame implements ActionListener {
     	populateMemoryTable();
     }
     
-    public void runInstructionLoad() {
+    private void runHalt() {
+    	
+    	System.out.println("HALT BUTTON CLICKED!");
+    	
+    	//Do something
+    }
+    
+    private void runInstructionLoad() {
     	
     	System.out.println("INSTRUCTION LOAD BUTTON CLICKED!");
-		//System.out.println(getInstructionWord());
+		System.out.println(getInstructionWord());
 		
 		cpu.loadToggleInstruction(getInstructionWord());
 		populateMemoryTable();
 		//System.out.println(cpu.getMemory().get(MemoryLocation.RESERVED_ADDRESS_TOGGLE_INSTRUCTION).getValue().getValue());
     }
     
-    public void runSingleStep() {
+    private void runSingleStep() {
     	
     	System.out.println("SINGLE STEP BUTTON CLICKED!");
 		System.out.println(getPcInput());
