d396e44540242e317a70a22b81138b3f2509c50b
griffith_benjamin <griffith_benjamin@bah.com>
2015-10-10 23:39:48 -0400

    Update with ALU functions AND, OR, & NOT

--- src/ArithmeticLogicUnit.java
+++ src/ArithmeticLogicUnit.java
@@ -114,4 +114,164 @@ public final class ArithmeticLogicUnit {
 	{
 		return String.format("%016d", Integer.parseInt(value));
 	}
+	/**
+	 * Logical AND of RegisterP and RegisterQ
+	 * @param p : which is a bitString
+	 * @param q : which is a bitString
+	 * @return c(p) <- c(p) AND c(q)
+	 */		
+	public static void and(String p, String q)
+	{
+	//register will be of length 16 bits
+		int n = 16;
+		for(int i = 0;i<n;i++)
+		{
+			String pbit = p.substring(i, i+1);
+			String qbit = q.substring(i, i+1);
+
+			String r = pbit + qbit;
+			if (i==0)
+			{
+				//edge case: first bit
+				if (r == "00" || r == "01" || r == "10")
+				{
+					p= "0" + p.substring(i+1, i+2);
+				}
+				else if (r == "11")
+				{
+					p= "1" + p.substring(i+1, i+2);
+				}
+
+			}
+			else if (i==n)
+			{// edge case:  last bit
+				if (r == "00" || r == "01" || r == "10")
+				{
+					p= p.substring(i-1, i)+"0";
+				}
+				else if (r == "11")
+				{
+					p= p.substring(i-1, i)+"1";
+				}
+
+			}
+			else
+			{//general case
+				if (r == "00" || r == "01" || r == "10")
+				{
+					p= p.substring(i-1, i)+"0"+ p.substring(i+1, i+2);
+				}
+				else if (r == "11")
+				{
+					p= p.substring(i-1, i)+"1"+ p.substring(i+1, i+2);
+				}
+			}
+		}
+	}
+	
+	
+	/**
+	 * Logical OR of RegisterP and RegisterQ
+	 * @param p : which is a bitString
+	 * @param q : which is a bitString
+	 * @return c(p) <- c(p) OR c(q)
+	 */	
+	public static void or(String p, String q)
+	{
+	//register will be of length 16 bits
+		int n = 16;
+		for(int i = 0;i<n;i++)
+		{
+			String pbit = p.substring(i, i+1);
+			String qbit = q.substring(i, i+1);
+
+			String r = pbit + qbit;
+			if (i==0)
+			{
+				//edge case: first bit
+				if (r == "11" || r == "01" || r == "10")
+				{
+					p= "1" + p.substring(i+1, n);
+				}
+				else if (r == "00")
+				{
+					p= "0" + p.substring(i+1, n);
+				}
+
+			}
+			else if (i==n)
+			{// edge case:  last bit
+				if (r == "11" || r == "01" || r == "10")
+				{
+					p= p.substring(0, i)+"1";
+				}
+				else if (r == "00")
+				{
+					p= p.substring(0, i)+"0";
+				}
+
+			}
+			else
+			{//general case
+				if (r == "11" || r == "01" || r == "10")
+				{
+					p= p.substring(0, i)+"1"+ p.substring(i+1, n);
+				}
+				else if (r == "00")
+				{
+					p= p.substring(0, i)+"0"+ p.substring(i+1, n);
+				}
+			}
+		}
+	}
+	/**
+	 * Logical NOT of RegisterP ; i.e. switch "1's & 0's"
+	 * @param p : which is a bitString
+	 * @return c(p) <- NOT c(p) 
+	 */		
+	public static void not(String p)
+	{
+	//register will be of length 16 bits
+		int n = 16;
+		for(int i = 0;i<n;i++)
+		{
+			String r = p.substring(i, i+1);
+			if (i==0)
+			{
+				//edge case: first bit
+				if (r == "1")
+				{
+					p= "0" + p.substring(i+1, n); //switch the first bit, save the rest
+				}
+				else if (r == "0")
+				{
+					p= "1" + p.substring(i+1, n);
+				}
+
+			}
+			else if (i==n)
+			{// edge case:  last bit
+				if (r == "0")
+				{
+					p= p.substring(0, i)+"1"; //save everything, but switch the last bit
+				}
+				else if (r == "1")
+				{
+					p= p.substring(0, i)+"0";
+				}
+
+			}
+			else
+			{//general case
+				if (r == "0" )
+				{
+					p= p.substring(0, i)+"1"+ p.substring(i+1, n); //change the ith bit
+				}
+				else if (r == "1")
+				{
+					p= p.substring(0, i)+"0"+ p.substring(i+1, n);
+				}
+			}
+		}
+	}	
 }
